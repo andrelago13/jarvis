73cb36f871523d190d2c5e7d03e24e28ea302f48
André Lago <andrelago1995@gmail.com>
2018-02-02 00:23:24 +0000

    Ping service and artifact fix

---
+++ jarvis-backend/src/routes/Ping.java
@@ -0,0 +1,14 @@
+package routes;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/ping")
+public class Ping {
+    @GET
+    @Produces("text/plain")
+    public String getClichedMessage() {
+        return "I am alive! :D";
+    }
+}
