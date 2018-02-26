70c835d9961eca154ec121c571b40de3790d3e4b
André Lago <andrelago1995@gmail.com>
2018-02-26 11:37:59 +0000

    Updated Slack messages

--- jarvis-backend/src/jarvis/util/AdminAlertUtil.java
+++ jarvis-backend/src/jarvis/util/AdminAlertUtil.java
@@ -3,9 +3,9 @@ package jarvis.util;
 import slack.SlackUtil;
 
 public class AdminAlertUtil {
-    private static final String MSG_JARVIS_DEPLOYED = "Jarvis was successfully deployed!";
-    private static final String MSG_NO_DB_CONNECTION = "There seems to be no connection to the database.";
-    private static final String MSG_DB_ERROR = "There was an error with the Jarvis DB: ";
+    private static final String MSG_JARVIS_DEPLOYED = "Jarvis was successfully deployed! :clap: :tada:";
+    private static final String MSG_NO_DB_CONNECTION = "There seems to be no connection to the database. :warning:";
+    private static final String MSG_DB_ERROR = "There was an error with the Jarvis DB: :fire: :fire: :fire:";
 
     public static boolean alertJarvisDeployed() {
         return makeProductionAlert(MSG_JARVIS_DEPLOYED);
