439482c5a5771cfada98aceebdcf9a530d9bdb20
André Lago <andrelago1995@gmail.com>
2018-03-06 21:47:10 +0000

    Time parsing test

--- jarvis-backend/src/jarvis/routes/Index.java
+++ jarvis-backend/src/jarvis/routes/Index.java
@@ -1,9 +1,7 @@
 package jarvis.routes;
 
-import jarvis.controllers.OnOffLight;
-import jarvis.controllers.definitions.Thing;
-import mongodb.MongoDB;
 import org.json.JSONObject;
+import slack.SlackUtil;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,6 +9,12 @@ import javax.ws.rs.Produces;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Path("/")
 public class Index {
@@ -26,7 +30,22 @@ public class Index {
             }
             JSONObject obj = new JSONObject();
             obj.put("key", "value");
-            return obj.toString();
+
+            ScheduledExecutorService executor =
+                    Executors.newSingleThreadScheduledExecutor();
+            executor.schedule(new Runnable() {
+                @Override
+                public void run() {
+                    SlackUtil.sendIoTMessage("test schedule");
+                }
+            }, 10, TimeUnit.SECONDS);
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            String date = "2017-07-12T16:30:00Z";
+            date.replace('T', ' ');
+            Date parsedDate = dateFormat.parse(date.replace('T', ' '));
+            Timestamp timestamp = new java.sql.Timestamp(parsedDate.getTime());
+            return timestamp.toString();
         } catch (Exception e) {
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
