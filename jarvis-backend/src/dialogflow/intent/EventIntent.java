b66ea696ceef3389a0f1e743e89f2acc49f37e41
André Lago <andrelago1995@gmail.com>
2018-04-02 17:59:45 +0100

    [WIP] cancel events

--- jarvis-backend/src/dialogflow/intent/EventIntent.java
+++ jarvis-backend/src/dialogflow/intent/EventIntent.java
@@ -29,6 +29,7 @@ public class EventIntent extends DialogFlowIntent {
 
     @Override
     public QueryResponse execute() throws JarvisException {
+        // TODO check how to cancel dis thing
         Optional<JSONObject> optParameters = mRequest.getParameters();
         if (!optParameters.isPresent()) {
             return getErrorResponse();
