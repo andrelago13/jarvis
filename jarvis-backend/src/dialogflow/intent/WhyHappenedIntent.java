930b0f45fea6ac005cb8df8ba095bdabf24aa6f5
André Lago <andrelago1995@gmail.com>
2018-04-07 19:12:05 +0100

    Fetch latest command triggers from DB

--- jarvis-backend/src/dialogflow/intent/WhyHappenedIntent.java
+++ jarvis-backend/src/dialogflow/intent/WhyHappenedIntent.java
@@ -6,12 +6,11 @@ import dialogflow.intent.definitions.DialogFlowIntent;
 import dialogflow.intent.definitions.IntentExtras;
 import dialogflow.intent.subintents.ActionFinder;
 import jarvis.actions.command.definitions.Command;
-import jarvis.engine.JarvisEngine;
 import jarvis.events.definitions.EventHandler;
 import jarvis.util.JarvisException;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+import mongodb.MongoDB;
 import org.json.JSONObject;
 import res.Config;
 
@@ -19,6 +18,8 @@ public class WhyHappenedIntent extends DialogFlowIntent {
 
   public static final String INTENT_NAME = Config.DF_WHY_HAPPENNED_INTENT_NAME;
   public static final String INTENT_ID = Config.DF_WHY_HAPPENNED_INTENT_ID;
+
+  private static final int MAX_EVENTS_TO_FETCH = 20;
 
   public static final String MSG_ERROR = "Sorry, I'm not sure how to answer your question.";
   public static final String MSG_NO_ACTION = "I did not find any action that matches what you asked.";
@@ -45,15 +46,15 @@ public class WhyHappenedIntent extends DialogFlowIntent {
       return getNoActionResponse();
     }
     Optional<Command> optionalCommand = commandIntent.get().getCommand();
-    if(!optionalCommand.isPresent()) {
+    if (!optionalCommand.isPresent()) {
       return getNoActionResponse();
     }
 
     Command command = optionalCommand.get();
 
-    Set<EventHandler> handlers = JarvisEngine.getInstance().getEventHandlers();
-    for(EventHandler h : handlers) {
-      if(h.command.equals(command)) {
+    List<EventHandler> handlers = MongoDB.getLatestNEventsHandled(MAX_EVENTS_TO_FETCH);
+    for (EventHandler h : handlers) {
+      if (h.command.equals(command)) {
         return getSuccessResponse(h);
       }
     }
