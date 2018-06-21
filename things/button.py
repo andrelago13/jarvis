863ab017b64150b242f0f8a1b43df48238844365
André Lago <andrelago1995@gmail.com>
2018-06-21 09:56:32 +0100

    Button python script

---
+++ things/button.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+button_pin = 22
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+previous_state = True
+
+while True:
+    input_state = GPIO.input(button_pin)
+    if input_state != previous_state:
+        previous_state = input_state
+        if(input_state == False):
+            print('Button Pressed')
+            time.sleep(0.2)
